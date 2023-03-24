50790128896bf75e8a5660d95bdfc3ba38905b07
Christian Bollinger <christian.bollinger@hispeed.ch>
2023-03-24 17:35:52 +0100

    fixes elfeed setup

--- init.el
+++ init.el
@@ -1252,3 +1252,16 @@
   (elfeed-org)
   (setq rmh-elfeed-org-files (list "/home/christian/Daten/org-system/org-mode/refile/elfeed.org"))
 )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(org-attach-screenshot yasnippet-snippets xref-js2 which-key vterm use-package undo-tree typescript-mode treemacs-tab-bar treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired sourcemap restclient request rainbow-delimiters ox-reveal org-tree-slide org-present org-bullets no-littering lsp-ui lsp-treemacs lsp-ivy json-mode ivy-rich ivy-prescient indium hide-mode-line helpful gnuplot forge evil-nerd-commenter eterm-256color eshell-git-prompt elfeed-org doom-themes doom-modeline dired-single dired-open dired-hide-dotfiles counsel-projectile command-log-mode ccls auto-package-update all-the-icons-dired @)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
