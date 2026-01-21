64308a6199b1dc45d8ec3327845b212ae378b2c1
Christian Bollinger <christian.bollinger@duagon.com>
2026-01-21 12:41:32 +0100

    removes xcolor from configuration

--- init.el
+++ init.el
@@ -711,7 +711,6 @@
             ("T1" "fontenc"         t ("pdflatex"))
             ("" "fontspec"          t ("lualatex" "xelatex"))
             ("" "hyperref"          t ("pdflatex" "lualatex" "xelatex"))
-            ("table, dvipsnames, svgnames" "xcolor"       t ("pdflatex" "lualatex" "xelatex"))
   	  ("" "pifont"            t ("pdflatex" "lualatex" "xelatex"))
             ("tikz" "bclogo"        t ("pdflatex" "lualatex" "xelatex"))
             ("" "lipsum"            t ("pdflatex" "lualatex" "xelatex"))
